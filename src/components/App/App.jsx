38b8b28432edc87f4616c226f8fc36e6fe25ad1b
Alvin Graham <grah0071@gmail.com>
2024-01-24 20:03:22 -0600

    Setup router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,29 @@
-import axios from 'axios';
-import './App.css';
-import StudentForm from '../StudentForm/StudentForm.jsx';
-import StudentList from '../StudentList/StudentList.jsx';
+import axios from "axios";
+import { HashRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import StudentForm from "../StudentForm/StudentForm.jsx";
+import StudentList from "../StudentList/StudentList.jsx";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-            <h1 className="App-title">GitHub Student List</h1>
-            </header>
-            <br/>
-            <StudentForm />
-
-            <p>Student list:</p>
-            <StudentList />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Router>
+        <header className="App-header">
+          <h1 className="App-title">GitHub Student List</h1>
+        </header>
+        <br />
+        <Route
+          path="/"
+          exact>
+          <StudentForm />
+        </Route>
+        <Route path="/allStudents">
+          <p>Student list:</p>
+          <StudentList />
+        </Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
